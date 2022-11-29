ea3048c59e1fbe4304f8f24bca5639c819753b16
Brett McElvain <Brett.McElvain@vu.com>
2022-11-29 13:52:38 -0600

    Test

---
+++ Week10/Week10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Week10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, dynamic> inventory = new Dictionary<string, dynamic>();
+
+            inventory.Add("gold", 500);
+            List<string> pouchList = new List<string>() {"flint", "twine", "gemstone"};
+            inventory.Add("pouch", pouchList);
+            List<string> backpackList = new List<string>() { "xylophone", "dagger", "bedroll", "bread loaf" };
+            inventory.Add("backpack", backpackList);
+            List<string> pocketList = new List<string>() { "seashell", "strange berry", "lint" };
+            inventory.Add("pocket", pocketList);
+        }
+    }
+}
